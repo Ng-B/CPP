9604defaa81be03a5c647e2eba8d168c29cf1bb7
Naod Bekele <naod.bekele@epita.fr>
2022-11-24 18:11:47 +0100

    Set functions beginning

---
+++ set_functions/set_operations.hpp
@@ -0,0 +1,58 @@
+#include <set>
+
+std::set<int> operator+(const std::set<int>& s1,const std::set<int>& s2)
+{
+    std::set<int> new_set;
+    for (auto s : s1)
+    {
+        new_set.insert(s);
+    }
+    for (auto s : s2)
+    {
+        new_set.insert(s);
+    }
+    return new_set;
+}
+
+std::set<int> operator-(const std::set<int>& s1,const std::set<int>& s2)
+{
+    std::set<int> new_set;
+    for (auto s : s1)
+    {
+        new_set.insert(s);
+    }
+    for (auto s : s2)
+    {
+        new_set.erase(s);
+    }
+    return new_set;
+}
+
+
+std::set<int> operator+(const std::set<int>& s1,const std::set<int>& s2)
+{
+    std::set<int> new_set;
+    for (auto s : s1)
+    {
+        new_set.insert(s);
+    }
+    for (auto s : s2)
+    {
+        new_set.insert(s);
+    }
+    return new_set;
+}
+
+std::set<int> operator-(const std::set<int>& s1,const std::set<int>& s2)
+{
+    std::set<int> new_set;
+    for (auto s : s1)
+    {
+        new_set.insert(s);
+    }
+    for (auto s : s2)
+    {
+        new_set.erase(s);
+    }
+    return new_set;
+}
